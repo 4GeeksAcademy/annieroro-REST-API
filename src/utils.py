e8437741679a44835d2523f3b99fc7c5f7890da2
Annie Roldán Rodríguez <149087336+AnnieRoro@users.noreply.github.com>
2024-02-24 11:56:23 +0000

    wip

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,9 @@
 from flask import jsonify, url_for
 
+
 class APIException(Exception):
     status_code = 400
+
 
     def __init__(self, message, status_code=None, payload=None):
         Exception.__init__(self)
@@ -14,6 +16,7 @@ class APIException(Exception):
         rv = dict(self.payload or ())
         rv['message'] = self.message
         return rv
+
 
 def has_no_empty_params(rule):
     defaults = rule.defaults if rule.defaults is not None else ()
@@ -29,13 +32,13 @@ def generate_sitemap(app):
             url = url_for(rule.endpoint, **(rule.defaults or {}))
             if "/admin/" not in url:
                 links.append(url)
-
     links_html = "".join(["<li><a href='" + y + "'>" + y + "</a></li>" for y in links])
     return """
         <div style="text-align: center;">
-        <img style="max-height: 80px" src='https://storage.googleapis.com/breathecode/boilerplates/rigo-baby.jpeg' />
-        <h1>Rigo welcomes you to your API!!</h1>
-        <p>API HOST: <script>document.write('<input style="padding: 5px; width: 300px" type="text" value="'+window.location.href+'" />');</script></p>
-        <p>Start working on your proyect by following the <a href="https://start.4geeksacademy.com/starters/flask" target="_blank">Quick Start</a></p>
-        <p>Remember to specify a real endpoint path like: </p>
-        <ul style="text-align: left;">"""+links_html+"</ul></div>"
+            <img style="max-height: 80px" src='https://storage.googleapis.com/breathecode/boilerplates/rigo-baby.jpeg' />
+            <h1>Rigo welcomes you to your API!!</h1>
+            <p>API HOST: <script>document.write('<input style="padding: 5px; width: 300px" type="text" value="'+window.location.href+'" />');</script></p>
+            <p>Start working on your proyect by following the <a href="https://start.4geeksacademy.com/starters/flask" target="_blank">Quick Start</a></p>
+            <p>Remember to specify a real endpoint path like: </p>
+            <ul style="text-align: left;">""" + links_html + """</ul>
+        </div>"""
